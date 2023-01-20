c1ca5e59a290c4b534d32d984723fe50f297edc7
Eric Sondergard <4650644+esond@users.noreply.github.com>
2023-01-20 15:33:14 -0600

    Add configuration extensions project

---
+++ source/Xerris.Extensions.Configuration/ConfigurationException.cs
@@ -0,0 +1,9 @@
+﻿namespace Xerris.Extensions.Configuration;
+
+public class ConfigurationException : Exception
+{
+    public ConfigurationException(string message)
+        : base(message)
+    {
+    }
+}
