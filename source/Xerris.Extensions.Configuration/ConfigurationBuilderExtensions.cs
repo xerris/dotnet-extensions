2781b2ea066c6a63652632952f22270d8a0505d4
Eric Sondergard <4650644+esond@users.noreply.github.com>
2023-01-20 15:58:01 -0600

    Add tests for configuration extensions

--- source/Xerris.Extensions.Configuration/ConfigurationBuilderExtensions.cs
+++ source/Xerris.Extensions.Configuration/ConfigurationBuilderExtensions.cs
@@ -6,14 +6,14 @@ namespace Xerris.Extensions.Configuration;
 public static class ConfigurationBuilderExtensions
 {
     /// <summary>
-    /// Add new configuration keys for each existing key, substituting delimiters with those supplied.
+    /// Add new configuration keys for each existing configuration key, substituting delimiters with those supplied.
     /// </summary>
     /// <param name="builder">The configuration builder.</param>
     /// <param name="keyDelimiters">The configuration setting delimiters.</param>
     /// <returns>The configuration builder.</returns>
     /// <remarks>
-    /// Given the <paramref name="keyDelimiters" />: <c>":", "_", "-"</c> an existing  key of <c>Foo:Bar</c> will
-    /// have duplicate values added for the keys: <c>"Foo_Bar"</c> and <c>"Foo-Bar"</c>.
+    /// Given the <paramref name="keyDelimiters" />: <c>":", "_", "-"</c> an existing key of <c>Foo:Bar</c> will
+    /// have duplicate values added for the keys <c>"Foo_Bar"</c> and <c>"Foo-Bar"</c>.
     /// <br /><br />
     /// It is important that this is the last configuration added in the configuration chain. Any configuration
     /// keys that are added afterward will not have their delimiters substituted.
