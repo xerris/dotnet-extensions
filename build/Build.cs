f36c82073ea2d655adad89d83e00ca67b20f469b
Eric Sondergard <4650644+esond@users.noreply.github.com>
2023-01-20 16:09:06 -0600

    Compile solution with NUKE

--- build/Build.cs
+++ build/Build.cs
@@ -1,15 +1,17 @@
-using System;
-using System.Linq;
+using System.IO;
+using System.Text.Json;
 using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Serilog;
 using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+// ReSharper disable InconsistentNaming
 
 class Build : NukeBuild
 {
@@ -21,24 +23,50 @@ class Build : NukeBuild
 
     public static int Main () => Execute<Build>(x => x.Compile);
 
+    protected override void OnBuildInitialized() =>
+        Log.Information("{VersionInfo}", JsonSerializer.Serialize(GitVersion));
+
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Required] [GitVersion(Framework = "net6.0", NoFetch = true)] readonly GitVersion GitVersion;
+
+    [CI] readonly GitHubActions GitHubActions;
+
+    [Solution] readonly Solution Solution;
+
+    AbsolutePath OutputDirectory => RootDirectory / "output";
 
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            DotNetClean(s => s
+                .SetProject(Solution));
+
+            EnsureCleanDirectory(OutputDirectory);
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
-        .DependsOn(Restore)
+        .DependsOn(Clean, Restore)
         .Executes(() =>
         {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .EnableNoRestore());
+
+            File.WriteAllText(OutputDirectory / "VERSION", GitVersion.NuGetVersionV2);
         });
 
 }
