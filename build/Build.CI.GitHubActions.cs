86632276cabe213a11f1139cd42d2dd2cb9a7913
Eric Sondergard <4650644+esond@users.noreply.github.com>
2023-03-31 16:17:32 -0600

    Add release workflow and set SDK version to 6.0.407

--- build/Build.CI.GitHubActions.cs
+++ build/Build.CI.GitHubActions.cs
@@ -10,8 +10,18 @@ using Xerris.Nuke.Components;
     OnPullRequestBranches = new[] { "main" },
     OnPushBranches = new[] { "main", "release/v*" },
     PublishArtifacts = true,
-    InvokedTargets = new[] { nameof(ITest.Test), nameof(IPack.Pack) },
+    InvokedTargets = new[] { nameof(ITest.Test) },
     CacheKeyFiles = new[] { "global.json", "src/**/*.csproj" })]
+// TODO: Package signing
+[GitHubActions(
+    "release",
+    GitHubActionsImage.UbuntuLatest,
+    FetchDepth = 0,
+    OnPushTags = new[] { "v*" },
+    PublishArtifacts = true,
+    InvokedTargets = new[] { nameof(ITest.Test), nameof(IPack.Pack), nameof(IPush.Push) },
+    CacheKeyFiles = new[] { "global.json", "src/**/*.csproj" },
+    ImportSecrets = new[] { nameof(IPush.NuGetApiKey) })]
 partial class Build
 {
 }
--- build/Build.cs
+++ build/Build.cs
@@ -54,7 +54,6 @@ partial class Build : NukeBuild,
     Target IPush.Push => _ => _
         .Inherit<IPush>()
         .Consumes(this.FromComponent<IPush>().Pack)
-        .Requires(() =>
-            this.FromComponent<IHasGitRepository>().GitRepository.Tags.Any())
+        .Requires(() => this.FromComponent<IHasGitRepository>().GitRepository.Tags.Any())
         .WhenSkipped(DependencyBehavior.Execute);
 }
